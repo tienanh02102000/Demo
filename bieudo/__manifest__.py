43941d30c0524b63a1bb43428afe2b884d0370ad
Tran Tien Anh <trantienanhbtec@hmail.com>
2019-12-14 08:48:07 +0700

    Demo

---
+++ bieudo/__manifest__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "bieudo",
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+    ],
+}
---
+++ bieudo/models/models.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class bieudo(models.Model):
+    _name = 'bieudo.bieudo'
+
